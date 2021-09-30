b2e064e0065c3670e4f93a2cbb1b803a2a104164
谢小希 <184415675@qq.com>
2021-09-30 17:56:28 +0800

    0930-admin

--- src/webConfig.js
+++ src/webConfig.js
@@ -2,6 +2,6 @@ module.exports={
     "webName": "机将",
     "webICP": "粤ICP备19047662号",
     "qiniuHost": "https://img.hqjiankang.com/",
-    "version": 168,
-    "versionTime": "2021-09-23T06:04:28.993Z"
+    "version": 169,
+    "versionTime": "2021-09-24T08:18:36.659Z"
 }
